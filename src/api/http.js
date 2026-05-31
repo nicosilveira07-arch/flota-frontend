887f693604839fe6b316523aa13619df00cb42d0
Nicolas Silveira <nicosilveira07@gmail.com>
2026-05-30 21:10:32 -0300

    Remove request debug logs

--- src/api/http.js
+++ src/api/http.js
@@ -7,7 +7,6 @@ const http = axios.create({
   }
 });
 
-// REQUEST INTERCEPTOR
 http.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
@@ -18,31 +17,20 @@ http.interceptors.request.use(
 
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
-// RESPONSE INTERCEPTOR
-http.interceptors.request.use(
-  (config) => {
-    console.log("REQUEST:", {
-      baseURL: config.baseURL,
-      url: config.url,
-      completa: `${config.baseURL}${config.url}`,
+http.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.log("API ERROR:", {
+      message: error.message,
+      status: error.response?.status || "NO_STATUS",
+      data: error.response?.data || "NO_RESPONSE"
     });
 
-    const token = localStorage.getItem("token");
-
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-  },
-  (error) => {
     return Promise.reject(error);
   }
 );
-console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
+
 export default http;
